0722a724c4967338733fb2fd9bad315fd01215d1
aadeleke4 <aadeleke4@academic.rrc.ca>
2024-06-06 21:23:09 -0500

    updated logout code to Ensure no further code is executed after the redirect

--- model/logout.php
+++ model/logout.php
@@ -1,5 +1,6 @@
 <?php
-    session_start();
-    session_destroy();
-    header('Location: ../view/login.php');
+    session_start(); // Start the session to access session variables
+    session_destroy(); // Destroy all data registered to a session
+    header('Location: ../view/login.php'); // Redirect the user to the login page
+    exit(); // Ensure no further code is executed after the redirect
 ?>
